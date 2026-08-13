a32931a19d3128e426126c56dedb32ce24075399
M4322uni <piscitello.2130445@studenti.uniroma1.it>
2026-08-13 19:49:07 +0200

    start on AST

---
+++ src/main/scala/logic/parsing/ProofAST.scala
@@ -0,0 +1,30 @@
+package logic.parsing
+
+import utils.NonBlankString
+
+case class Text(assumption: Seq[Expression], goals: Seq[Expression], proof: Proof)
+
+enum Expression:
+  case EqualityObj(left: ConstructedObj, right: ConstructedObj)
+  case EqualityMorph(left: ConstructedMorph, right: ConstructedMorph)
+  case JudgementObj(obj: ConstructedObj, typ: TypeObj)
+  case JudgementMorph(morph: ConstructedMorph, typ: TypeMorph)
+
+enum ConstructedObj:
+  case BaseObj(base: logic.parsing.Object)
+  case Domain(morph: ConstructedMorph)
+  case Codomain(morph: ConstructedMorph)
+
+enum ConstructedMorph:
+  case BaseMorph(base: Morphism)
+  case Identity(obj: ConstructedObj)
+  case Composition(left: ConstructedMorph, right: ConstructedMorph)
+
+enum TypeObj:
+  case TypeCat(cat: Category)
+
+enum TypeMorph:
+  case HomSet(cat: Category, dom: ConstructedObj, cod: ConstructedObj)
+
+case class Category(name: NonBlankString)
+
---
+++ src/main/scala/utils/NonBlankString.scala
@@ -0,0 +1,11 @@
+package utils
+
+opaque type NonBlankString = String
+
+object NonBlankString:
+  def apply(kernel: String): NonBlankString =
+    if kernel.isBlank then throw IllegalArgumentException("NonBlankString must be built " +
+      "upon a non-blank string")
+    else kernel
+
+  given Conversion[NonBlankString, String] = identity
