ea3dec8829fca6822768eeb8056721c14495bbc4
M4322uni <piscitello.2130445@studenti.uniroma1.it>
2026-08-15 16:43:18 +0200

    continue work on parser

--- src/main/scala/logic/parsing/ParserFunctions.scala
+++ src/main/scala/logic/parsing/ParserFunctions.scala
@@ -23,21 +23,25 @@ def formula[$ : P]: P[Seq[Formula]] =
   P( ("\t" ~ (include | expression.map{ Formula.Expr(_) }) ~ space_trail ~ "\n").repX )
 
 def expression[$ : P]: P[Expression] =
-  P( ( (concatenation ~ indent_blank ~ "=" ~ indent_blank ~ concatenation)
-      .map { (c1: Concatenation, c2: Concatenation) => Equation(c1, c2) } 
-      | (concatenation ~ indent_blank ~ ":" ~ indent_blank ~ ttype)
-    )
+  P( (concatenation ~ indent_blank ~ "=" ~ indent_blank ~ concatenation)
+    .map { (c1: Concatenation, c2: Concatenation) => Equation(c1, c2) }
+    | (concatenation ~ indent_blank ~ ":" ~ indent_blank ~ ttype)
+    .map { (c: Concatenation, t: Type) => Judgement(c, t) }
   )
   
-def ttype[$ : P]: P[Unit] = ???
+def ttype[$ : P]: P[Type] = ???
   
 def concatenation[$ : P]: P[Concatenation] =
+  P( construction ~ (indent_blank ~ "+" ~ indent_blank ~ construction).repX )
+    .map { (c: Construction, s: Seq[Construction]) => Concatenation(c +: s) }
+
+def construction[$ : P]: P[Construction] =
   ???
 
 def indent_blank[$ : P]: P[Unit] = ???
 
 def include[$ : P]: P[Formula.Include] =
-  P( "<include " ~ (CharPred(_ != ' ') | " " ~ !">").repX(1).! ~ " >" )
+  P( "<include " ~ CharPred(c => c != '>' && (!c.isWhitespace || c == ' ')).repX(1).! ~ ">" )
     .map { Include(_) }
 
 def proof[$ : P]: P[Seq[ProofStep]] =
