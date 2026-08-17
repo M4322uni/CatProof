bdbfd3930a7e57cdae02f9668c1726fafb16349a
M4322uni <piscitello.2130445@studenti.uniroma1.it>
2026-08-17 21:00:35 +0200

    before dinner

--- src/main/scala/logic/parsing/ParserFunctions.scala
+++ src/main/scala/logic/parsing/ParserFunctions.scala
@@ -2,6 +2,7 @@ package logic.parsing
 
 import fastparse.*
 import NoWhitespace.*
+import logic.parsing.Category.*
 import logic.parsing.Construction.*
 import logic.parsing.Expression.*
 import logic.parsing.Formula.*
@@ -49,11 +50,15 @@ def construction[$ : P]: P[Construction] =
     | "cod(" ~ indent_blank ~ concatenation.map { Cod.apply } ~ indent_blank ~ ")"
     | "id(" ~ indent_blank ~ construction.map { Id.apply } ~ indent_blank ~ ")")
 
-def category[$ : P]: P[Category] = ???
+def category[$ : P]: P[Category] =
+  P( name ).map { Base.apply }
 
-def name[$ : P]: P[Name] = ???
+def name[$ : P]: P[Name] =
+  P( CharIn("A-Za-z_").! ~ CharsWhileIn("A-Za-z0-9_", 0).! )
+    .map { (s1: String, s2: String) => s1 + s2 }
 
-def indent_blank[$ : P]: P[Unit] = ???
+def indent_blank[$ : P]: P[Unit] =
+  P( ("\t" | " " | "\n" ~ (" " | "\n").repX ~ "\t").repX )
 
 def include[$ : P]: P[Formula.Include] =
   P( "<include " ~ name ~ ">" )
@@ -64,8 +69,16 @@ def proof[$ : P]: P[Seq[ProofStep]] =
     .map { (str: Seq[(Rule, Seq[Positive])]) =>
       str.map { (rule: Rule, refs: Seq[Positive]) => ProofStep(rule, refs) } }
   
-def rule[$ : P]: P[Rule] = ???
+def rule[$ : P]: P[Rule] =
+  P( "composition".map {_ => Rule.COMPOSITION} |
+    "transitivity".map {_ => Rule.TRANSITIVITY} )
 
-def reference[$ : P]: P[Seq[Positive]] = ???
+def reference[$ : P]: P[Seq[Positive]] =
+  P( "with " ~ line ~ (", " ~ line).repX )
+    .map { (p: Positive, s: Seq[Positive]) => p +: s }
+  
+def line[$ : P]: P[Positive] =
+  P( CharIn("1-9").! ~ CharsWhileIn("0-9").! )
+    .map { (s1: String, s2: String) => (s1 + s2).toInt }
 
 def space_trail[$ : P]: P[Unit] = ???
