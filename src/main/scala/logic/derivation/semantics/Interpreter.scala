6d2d71ef5e248145753e00ea771db095710722bb
M4322uni <piscitello.2130445@studenti.uniroma1.it>
2026-08-21 21:23:46 +0200

    work on interpreter

--- src/main/scala/logic/derivation/semantics/Interpreter.scala
+++ src/main/scala/logic/derivation/semantics/Interpreter.scala
@@ -2,14 +2,21 @@ package logic.derivation.semantics
 
 import logic.derivation.Diagram
 import logic.derivation.derivationTree.*
-import logic.parsing.*
+import logic.derivation.derivationTree.Condition.TypeJudgement
+import logic.parsing.{Concatenation, Construction, Expression, Formula, NameBound}
+import logic.parsing.Construction.*
+import logic.parsing.NameBound.*
 import logic.parsing.Formula.*
-//import logic.parsing.Expression.*
-import logic.derivation.derivationTree.Condition.*
+import logic.parsing.Type.*
+import logic.derivation.semantics.*
+import TranslateEncapsulate.*
 import utils.Name
 
+class SemanticError(message: String)
+  extends IllegalArgumentException(s"Semantic error: $message")
+
 def translateFormulaList(map: Map[Name, Diagram],
-                         types: Set[TypeJudgement],
+                         types: Map[Name, Type],
                          s: Seq[Formula]): Set[Condition] =
   s match
     case h :: tail =>
@@ -18,23 +25,23 @@ def translateFormulaList(map: Map[Name, Diagram],
     case Nil => Set()
 
 def translateFormula(map: Map[Name, Diagram],
-                     types: Set[TypeJudgement],
-                     f: Formula): (Set[Condition], Set[TypeJudgement]) =
+                     types: Map[Name, Type],
+                     f: Formula): (Set[Condition], Map[Name, Type]) =
   f match
     case Include(diagram) => translateDiagram(types, map.get(diagram) match
       case Some(d) => d
-      case _ => throw IllegalArgumentException(s"Semantic error: the $diagram diagram cannot be found")
+      case _ => throw SemanticError(s"the $diagram diagram cannot be found")
     )
     case Expr(exp) =>
       val (cond, type_res) = translateExpression(types, exp)
       (Set(cond), type_res)
 
-def translateDiagram(types: Set[TypeJudgement],
-                     diag: Diagram): (Set[Condition], Set[TypeJudgement]) =
+def translateDiagram(types: Map[Name, Type],
+                     diag: Diagram): (Set[Condition], Map[Name, Type]) =
   ???
 
-def translateExpression(types: Set[TypeJudgement],
-                        e: Expression): (Condition, Set[TypeJudgement]) =
+def translateExpression(types: Map[Name, Type],
+                        e: Expression): (Condition, Map[Name, Type]) =
   e match
     case logic.parsing.Expression.Equation(left, right)
       => (Condition.Equation(EquationPair(
@@ -42,24 +49,71 @@ def translateExpression(types: Set[TypeJudgement],
         translateConcatenation(types, right))), types)
     case logic.parsing.Expression.TypeJudgement(subj, typ)
       =>
-      val judge: TypeJudgement = Condition.TypeJudgement(subj, translateType(typ))
-      (judge, extendTypes(types, judge))
+      val (translatedType, newTypes) = translateType(types, typ)
+      val judge: TypeJudgement = Condition.TypeJudgement(subj, translatedType)
+      (judge, extendTypes(newTypes, judge))
 
-def extendTypes(types: Set[TypeJudgement], newType: TypeJudgement): Set[TypeJudgement]
-  = ???
+def extendTypes(types: Map[Name, Type], judge: TypeJudgement): Map[Name, Type] =
+  val TypeJudgement(name, ttype) = judge
+  extendTypes(types, name, ttype)
 
-def translateConcatenation(types: Set[TypeJudgement],
+def extendTypes(types: Map[Name, Type], name: Name, ttype: Type): Map[Name, Type] =
+  types.get(name) match
+    case Some(other) => if other != ttype
+      then throw SemanticError(s"more than one type assigned to $name")
+      else types
+    case _ => types + (name -> ttype)
+
+def translateConcatenation(types: Map[Name, Type],
                            c: Concatenation): logic.derivation.semantics.Construction =
-  c.constructions.size match
-    case 0 => throw IllegalArgumentException("Parser error: a concatenation of zero elements was parsed")
-    case 1 => ???
-    case _ => ???
+  c.constructions match
+    case Nil => throw IllegalArgumentException("Parser error: a concatenation of zero elements was parsed")
+    case Seq(c) => translateConstruction(types, c)
+    case seq => Morphism.Concatenation(seq.map {
+      translateConstruction(types, _) match
+        case casted: Morphism => casted
+        case b => throw SemanticError(s"$b is expected to be a morphism but isn't")
+    })
 
-def translateConstruction(c: logic.parsing.Construction): logic.derivation.semantics.Construction =
-  ???
+def translateConstruction(types: Map[Name, Type],
+                          c: logic.parsing.Construction): logic.derivation.semantics.Construction =
+  c match
+    case Atomic(name) => translateNameBound(name) match
+      case CategoryCapsule(casted) => casted
+      case NameCapsule(casted) => types.get(casted) match
+        case Some(value) => value match
+          case _ : MorphismType => Morphism.Base(casted)
+          case _ : ObjectType => Object.Base(casted)
+          case _ : CategoryType => Category.Base(casted)
+        case _ => throw SemanticError(s"the type of $name is not defined before use")
+      case _ => throw NotImplementedError("Non-basic categories are not yet implemented")
+    case Dom(morph) => translateConcatenation(types, morph) match
+      case casted: Morphism => Object.Domain(casted)
+      case _ => throw SemanticError(s"the domain of $morph is undefined as it's not a morphism")
+    case Cod(morph) => translateConcatenation(types, morph) match
+      case casted: Morphism => Object.Codomain(casted)
+      case _ => throw SemanticError(s"the codomain of $morph is undefined as it's not a morphism")
+    case Id(obj) => translateConstruction(types, obj) match
+      case casted: Object => Morphism.Identity(casted)
+      case _ => throw SemanticError(s"the identity of $obj is undefined as it's not an object")
 
-def translateType(t: Type): logic.derivation.semantics.Type =
-  ???
+def translateType(types: Map[Name, Type], t: logic.parsing.Type):
+    (logic.derivation.semantics.Type, Map[Name, Type]) =
+  t match
+    case Cat(cat) => translateNameBound(cat) match
+      case NameCapsule(casted) => (ObjectType.Cat(Category.Base(casted)),
+        extendTypes(types, casted, CategoryType.-))
+      case CategoryCapsule(casted) => (ObjectType.Cat(casted), types)
+    case HomSet(cat, dom, cod) => translateNameBound(cat) match
+      case NameCapsule(casted) => (ObjectType.Cat(Category.Base(casted)),
+        extendTypes(types, casted, CategoryType.-))
+      case CategoryCapsule(casted) => (ObjectType.Cat(casted), types)
 
-def translateNameBound(n: NameBound): Name | Category =
-  ???
+enum TranslateEncapsulate:
+  case NameCapsule(name: Name)
+  case CategoryCapsule(cat: Category)
+
+def translateNameBound(n: NameBound): TranslateEncapsulate =
+  n match
+    case Base(name) => NameCapsule(name)
+    // TODO: extend
--- src/main/scala/logic/derivation/semantics/Type.scala
+++ src/main/scala/logic/derivation/semantics/Type.scala
@@ -1,9 +1,12 @@
 package logic.derivation.semantics
 
-type Type = MorphismType | ObjectType
+type Type = MorphismType | ObjectType | CategoryType
 
 enum MorphismType:
   case HomSet(cat: Category, dom: Object, cod: Object)
 
 enum ObjectType:
   case Cat(cat: Category)
+
+enum CategoryType:
+  case -
